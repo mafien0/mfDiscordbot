2ed9c41503586251a027b1b076f2d042ec3650be
mafien0 <mafien_0@tuta.io>
2026-03-03 21:25:26 +0500

    "err" => "error" rename

--- api/routes.js
+++ api/routes.js
@@ -31,8 +31,8 @@ function createRouter() {
 		try {
 			await sendEmbedMsg(createMessage(msgHeader, msgContent));
 			return res.status(200).json({ message: "Message sent successfully" });
-		} catch (err) {
-			console.error(err);
+		} catch (error) {
+			console.error(error);
 			return res.status(500).json({ error: "Internal server error" });
 		}
 	});
@@ -53,8 +53,8 @@ function createRouter() {
 		try {
 			await updateStatusMsg(field, value);
 			return res.status(200).json({ message: "Status updates successfully" });
-		} catch (err) {
-			console.error(err);
+		} catch (error) {
+			console.error(error);
 			return res.status(500).json({ error: "Internal server error" });
 		}
 	});
